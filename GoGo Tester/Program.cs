420c492a13edb33667151cd3ff77552e5723ba1b
fan <fan@a.cn>
2015-05-25 22:41:31 +0800

    v1

--- GoGo Tester/Program.cs
+++ GoGo Tester/Program.cs
@@ -18,6 +18,7 @@ namespace GoGo_Tester
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
         static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
             string dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
